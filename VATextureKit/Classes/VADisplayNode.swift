8e3fc7e19dbcbda7d1c3c93cd416ea2b44cef135
Volodymyr Andriienko <volodymyr.andriienko@wefuture.co.uk>
2023-02-18 14:54:20 +0200

    Base display node

---
+++ VATextureKit/Classes/VADisplayNode.swift
@@ -0,0 +1,17 @@
+//
+//  VADisplayNode.swift
+//  VATextureKit
+//
+//  Created by Volodymyr Andriienko on 18.02.2023.
+//
+
+import AsyncDisplayKit
+
+open class VADisplayNode: ASDisplayNode {
+
+    public override init() {
+        super.init()
+
+        automaticallyManagesSubnodes = true
+    }
+}
