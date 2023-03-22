1d0dbe54b41142c5d86ef73e5af1bf085bd369d6
Volodymyr Andriienko <volodymyr.andriienko@wefuture.co.uk>
2023-03-22 19:24:59 +0200

    Pods added

---
+++ Example/VATextureKit/Umbrella.swift
@@ -0,0 +1,11 @@
+//
+//  Umbrella.swift
+//  VATextureKit_Example
+//
+//  Created by Volodymyr Andriienko on 22.03.2023.
+//  Copyright © 2023 Volodymyr Andriienko. All rights reserved.
+//
+
+import VATextureKit
+
+typealias Obs = VATextureKit.Obs
