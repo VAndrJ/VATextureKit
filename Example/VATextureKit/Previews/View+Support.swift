797feeaac6ddd7967c75740b111cfb39b5de1bfb
Volodymyr Andriienko <volodymyr.andriienko@wefuture.co.uk>
2023-04-10 22:03:23 +0300

    Preview support

---
+++ Example/VATextureKit/Previews/View+Support.swift
@@ -0,0 +1,16 @@
+//
+//  View+Support.swift
+//  VATextureKit_Example
+//
+//  Created by Volodymyr Andriienko on 10.04.2023.
+//  Copyright © 2023 Volodymyr Andriienko. All rights reserved.
+//
+
+#if canImport(SwiftUI)
+import SwiftUI
+
+@available (iOS 13.0, *)
+extension View {
+    var anyView: AnyView { AnyView(self) }
+}
+#endif
