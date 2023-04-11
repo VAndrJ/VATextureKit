20df1a848fc17a04cbec1dae65af982064340158
Volodymyr Andriienko <volodymyr.andriienko@wefuture.co.uk>
2023-04-11 10:03:08 +0300

    Radial gradient snapshots

---
+++ Example/Tests/Snapshots/VARadialGradientNodeTests.swift
@@ -0,0 +1,33 @@
+//
+//  VARadialGradientNodeTests.swift
+//  VATextureKit_Tests
+//
+//  Created by VAndrJ on 11.04.2023.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import SnapshotTesting
+import VATextureKit
+
+class VARadialGradientNodeTests: XCTestCase {
+
+    func test_node_cusom() {
+        let sut = VARadialGradientNode(gradient: .custom(
+            startPoint: .init(x: 0.2, y: 0.2),
+            endPoint: .init(x: 0.6, y: 0.6)
+        ))
+        sut.update(colors: (.green, 0), (.orange, 1))
+
+        assertNodeSnapshot(matching: sut, size: .fixed(CGSize(same: 24)))
+    }
+
+    func test_node_default() {
+        [VARadialGradientNode.Gradient.centered, .bottomLeft, .bottomRight, .topLeft, .topRight].forEach {
+            let sut = VARadialGradientNode(gradient: $0)
+            sut.update(colors: (.green, 0), (.orange, 1))
+
+            assertNodeSnapshot(matching: sut, size: .fixed(CGSize(same: 24)), additions: "\($0)")
+        }
+    }
+}
