2c1fe4ce49ed32961937c466bacb94ff2db9c470
Volodymyr Andriienko <volodymyr.andriienko@wefuture.co.uk>
2023-04-14 15:33:02 +0300

    [Example] Support node functions

--- MoviesExample/MoviesExample/Protocols/ControllerNode.swift
+++ MoviesExample/MoviesExample/Protocols/ControllerNode.swift
@@ -12,4 +12,8 @@ import AsyncDisplayKit
 protocol ControllerNode: ASDisplayNode {
 
     func viewDidLoad(in controller: UIViewController)
+    func viewDidAppear(in controller: UIViewController, animated: Bool)
+    func viewWillAppear(in controller: UIViewController, animated: Bool)
+    func viewWillDisappear(in controller: UIViewController, animated: Bool)
+    func viewDidDisappear(in controller: UIViewController, animated: Bool)
 }
