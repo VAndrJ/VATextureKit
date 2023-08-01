e454613b177fac229bd6c279a9f5d0acf78e7d18
Volodymyr Andriienko <volodymyr.andriienko@wefuture.co.uk>
2023-08-01 15:24:03 +0300

    [Core] ASCollectionNode tests

---
+++ Example/Tests/Core/ASCollectionNodeTests.swift
@@ -0,0 +1,23 @@
+//
+//  ASCollectionNodeTests.swift
+//  VATextureKit_Tests
+//
+//  Created by Volodymyr Andriienko on 01.08.2023.
+//  Copyright © 2023 Volodymyr Andriienko. All rights reserved.
+//
+
+import XCTest
+@testable import VATextureKit_Example
+import VATextureKit
+
+class ASCollectionNodeTests: XCTestCase {
+
+    func test_direction() {
+        let node = VAListNode(
+            data: .init(listDataObs: Observable<[String]>.never(), cellGetter: { _ in ASCellNode () }),
+            layoutData: .init(layout: .default(parameters: .init(scrollDirection: .horizontal)))
+        )
+
+        XCTAssertTrue(node.isHorizontal)
+    }
+}
