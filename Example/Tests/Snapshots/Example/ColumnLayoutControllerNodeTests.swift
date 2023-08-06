4eba98f4c7ee8d894eefc1e9c6c7bfaa6d491fd5
Volodymyr Andriienko <volodymyr.andriienko@wefuture.co.uk>
2023-08-06 10:05:21 +0300

    [Example] Column layout snapshots

---
+++ Example/Tests/Snapshots/Example/ColumnLayoutControllerNodeTests.swift
@@ -0,0 +1,29 @@
+//
+//  ColumnLayoutControllerNodeTests.swift
+//  VATextureKit_Tests
+//
+//  Created by Volodymyr Andriienko on 06.08.2023.
+//  Copyright © 2023 Volodymyr Andriienko. All rights reserved.
+//
+
+import XCTest
+@testable import VATextureKit_Example
+import SnapshotTesting
+import VATextureKit
+
+class ColumnLayoutControllerNodeTests: XCTestCase {
+
+    func test_node() {
+        appContext.themeManager.setLightAsCustomTheme()
+        let sut = ColumnLayoutControllerNode()
+
+        assertNodeSnapshot(matching: sut, size: .freeHeightFixedWidth(375))
+    }
+
+    func test_node_dark_theme() {
+        appContext.themeManager.setDarkAsCustomTheme()
+        let sut = ColumnLayoutControllerNode()
+
+        assertNodeSnapshot(matching: sut, size: .freeHeightFixedWidth(375))
+    }
+}
