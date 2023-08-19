e7ad3143d9d20a1810d65714eed60d3059d2a28b
Volodymyr Andriienko <volodymyr.andriienko@wefuture.co.uk>
2023-08-19 15:20:58 +0300

    [Core] Spacer node snapshots

---
+++ Example/Tests/Snapshots/VASpacerNodeTests.swift
@@ -0,0 +1,72 @@
+//
+//  VASpacerNodeTests.swift
+//  VATextureKit_Tests
+//
+//  Created by Volodymyr Andriienko on 19.08.2023.
+//  Copyright © 2023 Volodymyr Andriienko. All rights reserved.
+//
+
+import XCTest
+import SnapshotTesting
+import VATextureKit
+
+class VASpacerNodeTests: XCTestCase {
+
+    func test_node_mid() {
+        let sut = VADisplayNode().apply {
+            $0.backgroundColor = .green
+        }
+        let topNode = VADisplayNode()
+            .sized(height: 4)
+            .apply {
+                $0.backgroundColor = .black
+            }
+        let bottomNode = VADisplayNode()
+            .sized(height: 4)
+            .apply {
+                $0.backgroundColor = .brown
+            }
+        let spacerNode = VASpacerNode()
+
+        sut.layoutSpecBlock = { _, _ in
+            Column {
+                topNode
+                spacerNode
+                bottomNode
+            }
+        }
+
+        assertNodeSnapshot(matching: sut, size: .fixed(CGSize(width: 24, height: 48)))
+    }
+
+    func test_node_proportional() {
+        let sut = VADisplayNode().apply {
+            $0.backgroundColor = .green
+        }
+        let topNode = VADisplayNode()
+            .sized(height: 4)
+            .apply {
+                $0.backgroundColor = .black
+            }
+        let bottomNode = VADisplayNode()
+            .sized(height: 4)
+            .apply {
+                $0.backgroundColor = .brown
+            }
+        let topSpacerNode = VASpacerNode(flexShrink: 0.1, flexGrow: 0.25)
+        let midSpacerNode = VASpacerNode(flexShrink: 0.2, flexGrow: 0.5)
+        let bottomSpacerNode = VASpacerNode(flexShrink: 0.3, flexGrow: 0.75)
+
+        sut.layoutSpecBlock = { _, _ in
+            Column {
+                topSpacerNode
+                topNode
+                midSpacerNode
+                bottomNode
+                bottomSpacerNode
+            }
+        }
+
+        assertNodeSnapshot(matching: sut, size: .fixed(CGSize(width: 24, height: 48)))
+    }
+}
