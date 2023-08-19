aef9048055489413ff1f56456ec3ea8a5887e617
Volodymyr Andriienko <volodymyr.andriienko@wefuture.co.uk>
2023-08-19 15:22:00 +0300

    [Core] Spacer node tests

--- Example/Tests/Snapshots/VASpacerNodeTests.swift
+++ Example/Tests/Snapshots/VASpacerNodeTests.swift
@@ -11,24 +11,24 @@ import SnapshotTesting
 import VATextureKit
 
 class VASpacerNodeTests: XCTestCase {
+    let topNode = VADisplayNode()
+        .sized(height: 4)
+        .apply {
+            $0.backgroundColor = .black
+        }
+    let bottomNode = VADisplayNode()
+        .sized(height: 4)
+        .apply {
+            $0.backgroundColor = .brown
+        }
+    let sut = VADisplayNode().apply {
+        $0.backgroundColor = .green
+    }
 
     func test_node_mid() {
-        let sut = VADisplayNode().apply {
-            $0.backgroundColor = .green
-        }
-        let topNode = VADisplayNode()
-            .sized(height: 4)
-            .apply {
-                $0.backgroundColor = .black
-            }
-        let bottomNode = VADisplayNode()
-            .sized(height: 4)
-            .apply {
-                $0.backgroundColor = .brown
-            }
         let spacerNode = VASpacerNode()
 
-        sut.layoutSpecBlock = { _, _ in
+        sut.layoutSpecBlock = { [self] _, _ in
             Column {
                 topNode
                 spacerNode
@@ -40,24 +40,11 @@ class VASpacerNodeTests: XCTestCase {
     }
 
     func test_node_proportional() {
-        let sut = VADisplayNode().apply {
-            $0.backgroundColor = .green
-        }
-        let topNode = VADisplayNode()
-            .sized(height: 4)
-            .apply {
-                $0.backgroundColor = .black
-            }
-        let bottomNode = VADisplayNode()
-            .sized(height: 4)
-            .apply {
-                $0.backgroundColor = .brown
-            }
         let topSpacerNode = VASpacerNode(flexShrink: 0.1, flexGrow: 0.25)
         let midSpacerNode = VASpacerNode(flexShrink: 0.2, flexGrow: 0.5)
         let bottomSpacerNode = VASpacerNode(flexShrink: 0.3, flexGrow: 0.75)
 
-        sut.layoutSpecBlock = { _, _ in
+        sut.layoutSpecBlock = { [self] _, _ in
             Column {
                 topSpacerNode
                 topNode
