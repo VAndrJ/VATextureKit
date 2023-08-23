7f39b502bf8a13df29b9f23ed0dbc049382dbec0
Volodymyr Andriienko <volodymyr.andriienko@wefuture.co.uk>
2023-08-23 21:18:47 +0300

    [Core] VAContainerButtonNode improvements

--- VATextureKit/Classes/Buttons/VAContainerButtonNode.swift
+++ VATextureKit/Classes/Buttons/VAContainerButtonNode.swift
@@ -7,20 +7,51 @@
 
 import AsyncDisplayKit
 
+/// `VAContainerButtonNode` is a subclass of `VAButtonNode` that includes a child node and handles button taps.
 public final class VAContainerButtonNode<Node: ASDisplayNode>: VAButtonNode {
+    /// The child node to be displayed within the container button.
     public let child: Node
+    /// The insets to apply around the child node.
+    public var insets: UIEdgeInsets {
+        didSet { setNeedsLayout() }
+    }
+    /// A closure to be executed when the highlighting state of the button changes.
+    public var onHighlight: ((_ child: Node, _ isHighlighted: Bool) -> Void)?
 
-    public init(child: Node, onTap: @escaping () -> Void) {
+    public override var isHighlighted: Bool {
+        didSet { onHighlight?(child, isHighlighted) }
+    }
+
+    /// Initializes the container button with a child node, insets, and a tap handler closure.
+    ///
+    /// - Parameters:
+    ///   - child: The child node to be displayed within the container button.
+    ///   - insets: The insets to apply around the child node.
+    ///   - onTap: The closure to be executed when the container button is tapped.
+    ///   - onHighlight: The closure to be executed when the highlighting state of the button changes.
+    public init(
+        child: Node,
+        insets: UIEdgeInsets = .zero,
+        onTap: (() -> Void)? = nil,
+        onHighlight: ((_ child: Node, _ isHighlighted: Bool) -> Void)? = nil
+    ) {
         self.child = child
+        self.insets = insets
 
         super.init()
 
-        self.onTap = onTap
+        if let onTap {
+            self.onTap = onTap
+        }
+        if let onHighlight {
+            self.onHighlight = onHighlight
+        }
         automaticallyManagesSubnodes = true
     }
 
     public override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
         child
-            .wrapped()
+            .padding(.insets(insets))
+            .centered()
     }
 }
