0f4763bed841c0b6ab225846127448e45abfb850
Volodymyr Andriienko <vandrjios@gmail.com>
2024-03-12 18:03:56 +0200

    [core] SwiftUI View hosting node snapshots.

---
+++ Example/Tests/Snapshots/VAViewHostingNodeTests.swift
@@ -0,0 +1,72 @@
+//
+//  VAViewHostingNodeTests.swift
+//  VATextureKit_Tests
+//
+//  Created by Volodymyr Andriienko on 12.03.2024.
+//  Copyright © 2024 Volodymyr Andriienko. All rights reserved.
+//
+
+import XCTest
+import SnapshotTesting
+import VATextureKit
+import SwiftUI
+
+@MainActor
+class VAViewHostingNodeTests: XCTestCase {
+
+    override func setUp() {
+        appContext.themeManager.setLightAsCustomTheme()
+    }
+
+    func test_sizing_height() {
+        let sut = VAViewHostingNode(
+            viewGetter: {
+                HStack {
+                    Text("Test")
+                    Spacer()
+                    Text("Test\nTest")
+                }
+            },
+            sizing: .viewHeight
+        )
+
+        assertNodeSnapshot(matching: sut, size: .freeHeightFixedWidth(320))
+    }
+
+    func test_sizing_width() {
+        let sut = VAViewHostingNode(
+            viewGetter: {
+                VStack {
+                    Text("Test")
+                    Spacer()
+                    Text("Test Test")
+                }
+            },
+            sizing: .viewWidth
+        )
+
+        assertNodeSnapshot(matching: sut, size: .freeWidthFixedHeight(100))
+    }
+
+    func test_sizing_size() {
+        let sut = VAViewHostingNode(
+            viewGetter: {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Test Test")
+                        Text("Test")
+                    }
+                    .background(.green)
+                    VStack(alignment: .trailing) {
+                        Text("Test")
+                        Text("Test Test")
+                    }
+                    .background(.orange)
+                }
+            },
+            sizing: .viewSize
+        )
+
+        assertNodeSnapshot(matching: sut, size: .auto)
+    }
+}
