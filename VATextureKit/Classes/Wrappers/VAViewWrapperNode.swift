ec74a8680a2e570683412a50a9f47ee24e66e850
Volodymyr Andriienko <vandrjios@gmail.com>
2024-04-09 18:46:33 +0300

    [core] VAViewWrapperNode corner parameters.

--- VATextureKit/Classes/Wrappers/VAViewWrapperNode.swift
+++ VATextureKit/Classes/Wrappers/VAViewWrapperNode.swift
@@ -30,12 +30,17 @@ open class VAViewWrapperNode<T: UIView>: VADisplayNode {
     /// - Parameters:
     ///   - actorChildGetter: A closure returning the UIView instance to be wrapped.
     ///   - sizing: The sizing option to apply to the wrapped view.
+    ///   - corner: Corner parameters.
     @available (iOS 13.0, *)
-    public init(actorChildGetter: @MainActor @escaping () -> T, sizing: Sizing? = nil) {
+    public init(
+        actorChildGetter: @MainActor @escaping () -> T,
+        sizing: Sizing? = nil,
+        corner: VACornerRoundingParameters = .default
+    ) {
         self.sizing = sizing
         self.childGetter = actorChildGetter
 
-        super.init()
+        super.init(corner: corner)
 
         switch sizing {
         case let .fixedWidth(width):
@@ -54,11 +59,16 @@ open class VAViewWrapperNode<T: UIView>: VADisplayNode {
     /// - Parameters:
     ///   - childGetter: A closure returning the UIView instance to be wrapped.
     ///   - sizing: The sizing option to apply to the wrapped view.
-    public init(childGetter: @escaping () -> T, sizing: Sizing? = nil) {
+    ///   - corner: Corner parameters.
+    public init(
+        childGetter: @escaping () -> T,
+        sizing: Sizing? = nil,
+        corner: VACornerRoundingParameters = .default
+    ) {
         self.sizing = sizing
         self.childGetter = childGetter
         
-        super.init()
+        super.init(corner: corner)
         
         switch sizing {
         case let .fixedWidth(width):
