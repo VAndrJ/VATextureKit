796eeedbd88f689b46cf6eab8772b0cb1ee0a5ec
Volodymyr Andriienko <vandrjios@gmail.com>
2024-06-05 15:42:49 +0300

    [core] Publisher to Infallible.

---
+++ Example/Tests/Core/Combine/PublisherToObservableTests.swift
@@ -0,0 +1,52 @@
+//
+//  PublisherToObservableTests.swift
+//  VATextureKit_Example
+//
+//  Created by VAndrJ on 05.06.2024.
+//  Copyright © 2024 Volodymyr Andriienko. All rights reserved.
+//
+
+import XCTest
+@testable import VATextureKit_Example
+import VATextureKitRx
+import RxBlocking
+import Combine
+
+class PublisherToObservableTests: XCTestCase {
+
+    func test_PublisherToObservable_sequence() throws {
+        let range = (1...10)
+        let publisher = range.publisher
+        let observable = publisher.asObservable()
+
+        let expected = Array(range)
+        let result = try observable.toBlocking().toArray()
+
+        XCTAssertEqual(expected, result)
+    }
+
+    func test_PublisherToObservable_error() throws {
+        let range = (1...10)
+        let publisher = range.publisher
+            .setFailureType(to: RxError.self)
+            .tryMap { _ -> Int in
+                throw RxError.noElements
+            }
+        let observable = publisher.asObservable()
+
+        XCTAssertThrowsError(try observable.toBlocking().toArray()) {
+            XCTAssertEqual(RxError.noElements.debugDescription, ($0 as! RxError).debugDescription)
+        }
+    }
+
+    func test_PublisherToInfallible_sequence() throws {
+        let range = (1...10)
+        let publisher = range.publisher
+        let observable = publisher.asInfallible()
+
+        let expected = Array(range)
+        let result = try observable.toBlocking().toArray()
+
+        XCTAssertEqual(expected, result)
+    }
+}
--- VATextureKit/ClassesRx/Combine+Rx.swift
+++ VATextureKit/ClassesRx/Combine+Rx.swift
@@ -27,16 +27,55 @@ public extension Publisher {
         Observable<Output>.create { observer in
             let cancellable = self
                 .sink(
-                    receiveCompletion: { completion in
-                        switch completion {
+                    receiveCompletion: {
+                        switch $0 {
                         case .finished:
                             observer.onCompleted()
                         case let .failure(error):
                             observer.onError(error)
                         }
                     },
-                    receiveValue: { value in
-                        observer.onNext(value)
+                    receiveValue: {
+                        observer.onNext($0)
+                    }
+                )
+
+            return Disposables.create {
+                cancellable.cancel()
+            }
+        }
+    }
+}
+
+public extension Publisher where Failure == Never {
+
+    /// Converts the current `Combine` `publisher` to an `RxSwift` `Infallible`.
+    ///
+    /// - Returns: An `Infallible` emitting elements from the `Combine` `publisher`.
+    ///
+    /// This method allows interoperability between `Combine` and `RxSwift` by converting a `Combine` publisher with a failure type of `Never` into an `Infallible`. The resulting `Infallible` emits the elements produced by the original publisher and completes when the publisher does.
+    ///
+    /// ### Example Usage:
+    /// ```swift
+    /// let combinePublisher = Just(5)
+    /// let infallible = combinePublisher.asInfallible()
+    /// infallible.subscribe { event in
+    ///     switch event {
+    ///     case .next(let value):
+    ///         print("Received value from Combine publisher: \(value)")
+    ///     case .completed:
+    ///         print("Combine publisher completed successfully.")
+    ///     }
+    /// }
+    func asInfallible() -> Infallible<Output> {
+        Infallible<Output>.create { observer in
+            let cancellable = self
+                .sink(
+                    receiveCompletion: { _ in
+                        observer(.completed)
+                    },
+                    receiveValue: {
+                        observer(.next($0))
                     }
                 )
 
