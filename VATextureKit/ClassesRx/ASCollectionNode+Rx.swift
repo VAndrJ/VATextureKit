2859a6d057de0ade41e591390741f6e855c55360
Volodymyr Andriienko <vandrjios@gmail.com>
2024-09-01 13:07:48 +0300

    [core] Swift 6 errors fix.

--- VATextureKit/ClassesRx/ASCollectionNode+Rx.swift
+++ VATextureKit/ClassesRx/ASCollectionNode+Rx.swift
@@ -5,10 +5,16 @@
 //  Created by Volodymyr Andriienko on 25.03.2023.
 //
 
+#if compiler(>=6.0)
+public import AsyncDisplayKit
+public import RxSwift
+public import RxCocoa
+#else
 import AsyncDisplayKit
-import VATextureKit
 import RxSwift
 import RxCocoa
+#endif
+import VATextureKit
 
 // swiftlint:disable indentation_width force_unwrapping implicitly_unwrapped_optional file_length
 extension Reactive where Base: ASCollectionNode {
