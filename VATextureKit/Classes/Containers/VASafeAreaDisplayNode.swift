a041ed40d6511be5f11a687b15494067ed56d114
Volodymyr Andriienko <vandrjios@gmail.com>
2024-09-02 11:03:37 +0300

    [core] Sendable conformance.

--- VATextureKit/Classes/Containers/VASafeAreaDisplayNode.swift
+++ VATextureKit/Classes/Containers/VASafeAreaDisplayNode.swift
@@ -7,7 +7,7 @@
 
 import AsyncDisplayKit
 
-open class VASafeAreaDisplayNode: VADisplayNode {
+open class VASafeAreaDisplayNode: VADisplayNode, @unchecked Sendable {
     
     public override init(corner: VACornerRoundingParameters = .default) {
         super.init(corner: corner)
